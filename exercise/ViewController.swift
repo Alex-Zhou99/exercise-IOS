f99ba94128c115bc2ecba044c2bc94946f90ae6a
shi-edward <shi_edward@icloud.com>
2016-07-18 13:42:21 -0500

    exercise basis

--- exercise/ViewController.swift
+++ exercise/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var moutainImage: UIImageView!
+    
+    @IBAction func buttonTapped(sender: AnyObject) {
+        moutainImage.hidden = false
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
